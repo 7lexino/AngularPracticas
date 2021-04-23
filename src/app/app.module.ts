593abfe804d718cb8004d3b1d9e6b4598943e41d
Alejandro Hernandez <alexhdzsolis@hotmail.com>
2021-04-23 00:38:17 -0500

    Mi primer componente generado con CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorld } from './primercomponente/primer.component';
+import { OtroComponenteComponent } from './otro-componente/otro-componente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorld
+    HelloWorld,
+    OtroComponenteComponent
   ],
   imports: [
     BrowserModule,
